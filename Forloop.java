8ccacd0e7b95af2e288b91da4ea2e45624aee6a6
Avdhut0 <ubaleavdhut@gmail.com>
2025-06-25 15:53:26 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,9 @@
+public class Forloop{
+    public static void main(String[] args) {
+        int i=0;
+      do{ 
+           System.out.println("java");
+           i++;
+      }while (i<=10);
+    }
+}
